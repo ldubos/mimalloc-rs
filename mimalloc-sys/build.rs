e4ffe2aefe7e55920d79d9553e4d4c3ccffb0a38
ldubos <ludwig.dubos@pm.me>
2024-09-03 15:17:28 +0200

    update mimalloc config

--- mimalloc-sys/build.rs
+++ mimalloc-sys/build.rs
@@ -2,6 +2,17 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    let os = {
+        let target = env::var("TARGET").unwrap();
+        let parts = target.split("-").collect::<Vec<&str>>();
+
+        if parts.len() >= 3 {
+            parts[2].to_string()
+        } else {
+            "unknown".to_string()
+        }
+    };
+
     let mut path: PathBuf = PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap());
     path.push("mimalloc");
 
@@ -17,9 +28,17 @@ fn main() {
     }
 
     build.define("MI_DEBUG", "0");
-
-    #[cfg(feature = "secure")]
     build.define("MI_SECURE", "4");
+
+    if os == "linux" {
+        build.define("MIMALLOC_ARENA_EAGER_COMMIT", "2");
+    } else {
+        build.define("MIMALLOC_ARENA_EAGER_COMMIT", "1");
+    }
+
+    build.define("MIMALLOC_ALLOW_LARGE_OS_PAGES", "0");
+    build.define("MIMALLOC_RESERVE_HUGE_OS_PAGES", "2");
+    build.define("MIMALLOC_PURGE_DELAY", "2500");
 
     if build.get_compiler().is_like_msvc() {
         build.cpp(true);
